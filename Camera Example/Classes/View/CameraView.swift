34f79b78ef349ea62f854b6cf7ec60ab42d40d0a
Muhammed Karakul <muhammedkarakul@gmail.com>
2019-08-06 11:29:15 +0300

    Initial Commit

---
+++ Camera Example/Classes/View/CameraView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraView.swift
+//  Camera Example
+//
+//  Created by Muhammed Karakul on 5.08.2019.
+//  Copyright © 2019 Muhammed Karakul. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Camera Example/Classes/ViewController/CameraViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CameraViewController.swift
+//  Camera Example
+//
+//  Created by Muhammed Karakul on 5.08.2019.
+//  Copyright © 2019 Muhammed Karakul. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+    private lazy var cameraView: UIView = {
+        CameraView()
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
